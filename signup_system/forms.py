5485aa5b4c160071679fb77f9b56fbca7a29de67
Neil Rutherford <neilinator@hotmail.com>
2020-01-15 10:04:54 +0800

    Add screening pages for US applicants and China employers

--- signup_system/forms.py
+++ signup_system/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-# Screening form that allows us to make assumptions about American applicants and save everyone money
+
 class USA_ApplicantScreen(FlaskForm):
         
     us_citizen = SelectField('Are you a citizen of the United States?', 
@@ -22,9 +22,9 @@ class USA_ApplicantScreen(FlaskForm):
                 ('1', "I'm in good health.")], 
             validators=[DataRequired()])
     
-    work_experience = SelectField('Do you have at least 2 years of experience working in an educational role and/or with children?', 
-            choices=[('1', 'Yes, I have two years of relevant work experience.'), 
-                ('0', "No, I don't have two years of relevant work experience.")], 
+    work_experience = SelectField('Do you have at least 2 years of work experience after graduation?', 
+            choices=[('1', 'Yes, I have two years of work experience.'), 
+                ('0', "No, I don't have two years of work experience.")], 
             validators=[DataRequired()])
     
     one_year = SelectField('Can you commit to a 1-year contract?', 
@@ -43,8 +43,93 @@ class USA_ApplicantScreen(FlaskForm):
 
     submit = SubmitField('Next >>')
 
-#class China_EmployerScreen(FlaskForm):
-    #sdffd
+
+class China_EmployerScreen(FlaskForm):
+    
+    business_license = SelectField('Do you have a business license?',
+            choices=[('1', "Yes, we do."), 
+                ('0', "No, we don't.")],
+            validators=[DataRequired()])
+
+    education_license = SelectField('Do you have an education license?',
+            choices=[('1', 'Yes, we do.'), 
+                ('0', "No, we don't.")],
+            validators = [DataRequired()])
+
+    z_visa = SelectField('Do you agree to provide the foreign teacher a Chinese work visa (Z visa)?',
+            choices=[('1', 'Yes, that is the only legal way to employ a foreigner.'), 
+                ('0', "What's a Z visa?")],
+            validators=[DataRequired()])
+    
+    one_year = SelectField('Are you willing to sign a 1-year contract?',
+            choices=[('1', 'Yes, we want long-term cooperation.'), 
+                ('0', 'No, we are looking for a short-term and/or part-time foreign teacher.')],
+            validators=[DataRequired()])
+
+    labor_law = SelectField("Will your contract be in accordance with the Labor Law of the People's Repbulic of China?",
+            choices=[('1', 'Yes, that is the only legal way to employ a foreigner.'), 
+                ('0', "China has labor laws?")],
+            validators=[DataRequired()])
+
+    max_hours = SelectField('Your foreign teacher will not work more than 44 hours per week. Is this acceptable for you?',
+            choices=[('1', 'Yes, our foreign teacher will not work more than 44 hours per week.'), 
+                ('0', 'No, our foreign teacher must work more than 44 hours per week.')],
+            validators=[DataRequired()])
+
+    min_salary = SelectField('If you are located in a first tier city, do you agree to pay your foreign teacher at least 15,000 RMB per month? If you are not located in a first tier city, do you agree to pay your foreign teacher at least 8,000 RMB per month? (This figure does not include benefits, such as rent, utilities, or airfare.)',
+            choices=[('1', 'Yes, we agree to pay our foreign teacher at least that much.'), 
+                ('0', 'No, we cannot agree to pay our foreign teacher that much.')],
+            validators=[DataRequired()])
+
+    payment_time = SelectField('Do you agree to pay your foreign teacher promptly on the first day of each month? (i.e. Payment for work done in October will be paid on October 1.)',
+            choices=[('1', 'Yes, we will pay our foreign teacher on the first of the month.'), 
+                ('0', 'No, we cannot guarantee that we can pay our employees on time.')],
+            validators=[DataRequired()])
+
+    direct_deposit = SelectField('Will you pay your foreign teacher by direct deposit?',
+            choices=[('1', 'Yes, we will pay our foreign teacher by direct deposit.'), 
+                ('0', 'No, we will pay our foreign teacher in cash.')],
+            validators=[DataRequired()])
+
+    housing = SelectField('Do you agree to provide housing for your foreign teacher?',
+            choices=[('1', 'Yes, we will provide housing for our foreign teacher.'), 
+                ('0', 'No, we cannot agree to do that.')],
+            validators=[DataRequired()])
+
+    medical_insurance = SelectField('Do you agree to provide medical insurance to your foreign teacher?',
+            choices=[('1', 'Yes, in accordance with Chinese law, we will provide medical insurance.'), 
+                ('0', 'No, we cannot agree to do that.')],
+            validators=[DataRequired()])
+
+    airfare = SelectField("Do you agree to pay for your foreign teacher's airfare to and from China at the beginning and end of their contract?",
+            choices=[('1', 'Yes, we agree to pay airfare to and from China.'), 
+                ('0', 'No, we cannot agree to that.')],
+            validators=[DataRequired()])
+
+    vacation = SelectField('Do you agree to give your foreign teacher at least 5 days of paid vacation per year (not including Chinese holidays)?',
+            choices=[('1', 'Yes, the foreign teacher will get at least 5 days of paid vacation.'), 
+                ('0', 'No, we cannot agree to that.')],
+            validators=[DataRequired()])
+
+    language = SelectField('The contract must be in both English and Chinese. The English text must match the Chinese text. Do you understand and accept this?',
+            choices=[('1', 'Yes, the foreign teacher should be able to read and understand what they are signing.'),
+                ('0', "No, we don't think this is important enough to find a translator for.")],
+            validators=[DataRequired()])
+
+    service_fee = SelectField("We do our best to screen applicants and ensure they meet our requirements. To contact one of our applicants, you must first pay us. This payment consists of visa reimbursement fees, a finder's fee, and a service fee. In the event that the applicant is denied a visa, we will refund the visa fees and the finder's fee. However, the service fee is non-refundable. Do you understand and accept this?",
+            choices=[('1', 'Yes, we understand and accept.'), 
+                ('0', "No, we are no longer interested in your service.")],
+            validators=[DataRequired()])
+
+    terms = SelectField("For many of our applicants, this is their first time leaving their home country. We want to make sure that they have a pleasant first experience working abroad, so we have certain expectations for our prospective employers. These expectations have been laid out very clearly, and you will accept all of them if you are to use our service. We take violations of these terms very seriously. If we find that you have proposed a contract that violates any of these terms, such as offering to pay 7,500 RMB per month, we have the right to terminate this agreement. We will remove you from our system, add you to our blacklist, and you will not receive any reimbursement. Do you understand and accept this?",
+            choices=[('1', 'Yes, we understand and accept.'), 
+                ('0', "No, we are no longer interested in your service.")],
+            validators=[DataRequired()])
+
+    #recaptcha = RecaptchaField()
+
+    submit = SubmitField('Next >>')
+
 
 #class USA_ApplicantSignup(FlaskForm):
     #dfsdfs
--- signup_system/routes.py
+++ signup_system/routes.py
@@ -1,39 +1,60 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask import current_app as app
-from signup_system.forms import USA_ApplicantScreen#, USA_ApplicantSignup
+from signup_system.forms import USA_ApplicantScreen, China_EmployerScreen#, USA_ApplicantSignup
 import uuid
+
 
 app = Blueprint('signup', __name__,
         template_folder = 'templates',
         static_folder = 'static')
 
 
+# Pre-qualifies US applicants and generates the client_id needed to sign up
 @app.route('/screen/usa-applicant', methods=['GET', 'POST'])
 def screen_usa_applicant():
     form = USA_ApplicantScreen()
-    # Generates a unique client ID / tracking number that is required to start sign-up form
     client_id = 'USA_APP_' + str(uuid.uuid4())
     if form.validate_on_submit():
         if form.us_citizen.data == '1' and form.college.data == '1' and form.criminal.data == '1' and form.medical.data == '1' and form.work_experience.data == '1' and form.one_year.data == '1' and form.other.data == '1' and form.terms.data == '1':
-            #return redirect('/')
-            # Pass client ID to sign up form to ensure that they agree to terms before signing up
             return redirect(url_for('signup.signup_usa_applicant', client_id = client_id))
         else:
-            return redirect(url_for('signup.screen_nope'))
+            return redirect(url_for('signup.screen_eng_nope'))
     return render_template('screen_usa_applicant.html', form=form)
 
 
-@app.route('/screen/nope', methods=['GET'])
-def screen_nope():
-    return render_template('screen_nope.html')
+# Pre-qualifies Chinese employers and generates the client_id needed to sign up
+@app.route('/screen/china-employer', methods=['GET', 'POST'])
+def screen_china_employer():
+    form = China_EmployerScreen()
+    client_id = 'CHI_EMP_' + str(uuid.uuid4())
+    if form.validate_on_submit():
+        if form.business_license.data == '1' and form.education_license.data == '1' and form.z_visa.data == '1' and form.one_year.data == '1' and form.labor_law.data == '1' and form.max_hours.data == '1' and form.min_salary.data == '1' and form.payment_time.data == '1' and form.direct_deposit.data == '1' and form.housing.data == '1' and form.medical_insurance.data == '1' and form.airfare.data == '1' and form.vacation.data == '1' and form.language.data == '1' and form.service_fee.data == '1' and form.terms.data == '1':
+            return redirect(url_for('signup.signup_china_employer', client_id = client_id))
+        else:
+            return redirect(url_for('signup.screen_chi_nope'))
+    return render_template('screen_china_employer.html', form=form)
 
 
-@app.route('/signup/usa_applicant', methods=['GET'])
+# Failed pre-qualification (English)
+@app.route('/screen/eng/nope', methods=['GET'])
+def screen_eng_nope():
+    return render_template('screen_eng_nope.html')
+
+
+# Failed pre-qualification (Chinese)
+@app.route('/screen/chi/nope', methods=['GET'])
+def screen_chi_nope():
+    return render_template('screen_chi_nope.html')
+
+
+# No client_id was provided, so redirect back to the screening page
+@app.route('/signup/usa-applicant', methods=['GET'])
 def redirect_screen_usa_applicant():
     return redirect(url_for('signup.screen_usa_applicant'))
 
 
-@app.route('/signup/usa_applicant/id=<client_id>', methods=['GET', 'POST'])
+# Client_id was provided, so start the signup process
+@app.route('/signup/usa-applicant/id=<client_id>', methods=['GET', 'POST'])
 def signup_usa_applicant(client_id):
     #form = USA_ApplicantSignup()
     if client_id:
@@ -43,15 +64,33 @@ def signup_usa_applicant(client_id):
         return "{}".format(client_id)
     else:
         return redirect(url_for('signup.screen_usa_applicant'))
+    return redirect(url_for('signup.screen_usa_applicant'))
 
-#@app.route('/signup/usa_affiliate')
+
+# No client_id was provided, so redirect back to the screening page
+@app.route('/signup/china-employer', methods=['GET'])
+def redirect_screen_china_employer():
+    return redirect(url_for('signup.screen_china_employer'))
+
+
+# Client_id was provided, so start the sign up process
+@app.route('/signup/china-employer/id=<client_id>', methods=['GET', 'POST'])
+def signup_china_employer(client_id):
+    if client_id:
+        return "{}".format(client_id)
+    else:
+        return redirect(url_for('signup.screen_china_employer'))
+    return redirect(url_for('signup.screen_china_employer'))
+
+
+#@app.route('/signup/usa-affiliate')
 #def signup_usa_affiliate():
 #    return '/////////////////////////////////////////////////'
 
-#@app.route('/signup/china_employer')
+#@app.route('/signup/china-employer')
 #def signup_china_employer():
 #    return '/////////////////////////////////////////////////////'
 
-#@app.route('/signup/china_affiliate')
+#@app.route('/signup/china-affiliate')
 #def signup_china_affiliate():
 #    return '/////////////////////////////////////////////////////'
